7b9b8ed84f68a91ebe23ace0b9cfff38ca510f35
jamesymat <40872027+jamesymat@users.noreply.github.com>
2024-02-15 15:00:25 -0800

    Update attempt1.ino

--- attempt1.ino
+++ attempt1.ino
@@ -1,5 +1,6 @@
+#include "Wire.h" //allows communication over i2c devices
 int mic = A0;
-
+int x;
 // Variable to hold analog values from mic
 int micOut;
 
@@ -8,27 +9,27 @@ void setup() {
   Wire.begin(9); //Start I2C as Salve on address 9
   Wire.onReceive(receiveEvent); 
 }
-void receiveEvent(int bytes) {
-  x = Wire.read(); //read from I2C
-}
 
-void loop() {
-    if (x > 0) {
-      char receivedChar = Serial.read();
-      Serial.print("Received: ");
-      Serial.println(receivedChar);
-        // read the input on analog pin 0:
-      micOut = analogRead(mic);
-
-      float voltage = micOut * (3.3 / 1024.0);
-
+void receiveEvent(int numBytes) {
+    while (Wire.available() > 0) {
+        int receivedChar = Wire.read();
+        Serial.print("Received: ");
+        Serial.println(receivedChar);
+        if (receivedChar > 0) {
+          micOut = analogRead(mic);
+          float voltage = micOut * (3.3 / 1024.0);
       // Print the voltage value to the serial monitor
-      Serial.print("ADC Reading: ");
-      Serial.print(micOut);
-      Serial.print(", Voltage: ");
-      Serial.println(voltage);
+          Serial.print("ADC Reading: ");
+          Serial.print(micOut);
+          Serial.print(", Voltage: ");
+          Serial.println(voltage);
+        } else {
+          Serial.println("Loading");
+      }
+    }
     }
 
+void loop() {
 
   // Delay for a short period before taking the next reading
   delay(500);
