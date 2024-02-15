13b50631074b4869655e5b1e427fee51c66b70f5
jamesymat <40872027+jamesymat@users.noreply.github.com>
2024-02-15 13:44:19 -0800

    Add files via upload

---
+++ attempt1.ino
@@ -0,0 +1,33 @@
+int mic = A0;
+
+// Variable to hold analog values from mic
+int micOut;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+      char receivedChar = Serial.read();
+      Serial.print("Received: ");
+      Serial.println(receivedChar);
+        // read the input on analog pin 0:
+      micOut = analogRead(mic);
+
+      float voltage = micOut * (3.3 / 1024.0);
+
+      // Print the voltage value to the serial monitor
+      Serial.print("ADC Reading: ");
+      Serial.print(micOut);
+      Serial.print(", Voltage: ");
+      Serial.println(voltage);
+    }
+
+
+  // Delay for a short period before taking the next reading
+  delay(500);
+  // print out the value you read:
+
+}
