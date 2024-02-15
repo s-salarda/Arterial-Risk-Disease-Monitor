a1af650ce6f06315fd3ddfff223cb2498be57f33
s-salarda <151248862+s-salarda@users.noreply.github.com>
2024-02-15 13:40:21 -0800

    Add files via upload

---
+++ sketch_feb11a.ino
@@ -0,0 +1,40 @@
+#include "Wire.h" //allows communication over i2c devices
+
+
+const int pressureInput = A0; //select the analog input pin for the pressure transducer
+const int pressureZero = 102.4; //analog reading of pressure transducer at 0psi
+const int pressureMax = 921.6; //analog reading of pressure transducer at 100psi
+const int pressuretransducermaxPSI = 100; //psi value of transducer being used
+const int baudRate = 9600; //constant integer to set the baud rate for serial monitor
+const int sensorreadDelay = 100; //constant integer to set the sensor read delay in milliseconds
+int x = 0;
+int arduino2 = 9;
+float pressureValue = 0; //variable to store the value coming from the pressure transducer
+
+void setup() //setup routine, runs once when system turned on or reset
+{
+  Wire.begin(); //statrt as master 
+  Serial.begin(baudRate); //initializes serial communication at set baud rate bits per second
+}
+
+void loop() //loop routine runs over and over again forever
+{
+  pressureValue = analogRead(pressureInput); //reads value from input pin and assigns to variable
+  pressureValue = ((pressureValue-pressureZero)*pressuretransducermaxPSI)/(pressureMax-pressureZero); //conversion equation to convert analog reading to psi
+  Serial.print(pressureValue, 1); //prints value from previous line to serial
+  Serial.println("psi"); //prints label to serial
+  delay(sensorreadDelay); //delay in milliseconds between read values
+
+//loop for when to start and end the 2nd arduino aka. sound sensor
+  if (pressureValue <= 180) {
+    x = 1;
+  } else {
+    x = 0;
+  }
+
+  Wire.beginTransmission(arduino2); //transmit to other arduino
+  Wire.write(x); //sends x
+  Wire.endTransmission(); //stop transmitting
+
+}
+
