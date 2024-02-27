e042ab01c3baff10e5f9d91f925b562161a0983c
jamesymat <40872027+jamesymat@users.noreply.github.com>
2024-02-27 13:40:54 -0800

    Update attempt1.ino

--- attempt1.ino
+++ attempt1.ino
@@ -1,8 +1,9 @@
 #include "Wire.h" //allows communication over i2c devices
 int mic = A0;
-int x;
+//int x;
 // Variable to hold analog values from mic
 int micOut;
+int time;
 
 void setup() {
   Serial.begin(9600);
@@ -13,16 +14,20 @@ void setup() {
 void receiveEvent(int numBytes) {
     while (Wire.available() > 0) {
         int receivedChar = Wire.read();
-        Serial.print("Received: ");
-        Serial.println(receivedChar);
+
         if (receivedChar > 0) {
           micOut = analogRead(mic);
           float voltage = micOut * (3.3 / 1024.0);
+          unsigned long currentTime = millis();
+          time = currentTime/1000;
+          Serial.print(time);
+          Serial.print(", ");
+          Serial.println(receivedChar);
       // Print the voltage value to the serial monitor
-          Serial.print("ADC Reading: ");
-          Serial.print(micOut);
-          Serial.print(", Voltage: ");
-          Serial.println(voltage);
+          //Serial.print("ADC Reading: ");
+          //Serial.print(micOut);
+          //Serial.print(", Voltage: ");
+          //Serial.println(voltage);
         } else {
           Serial.println("Loading");
       }
